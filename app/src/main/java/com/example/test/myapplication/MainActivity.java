97ab89fd63ddf1cb79d22af4f49ff1bf2f6b1d79
test <test@test.com>
2017-04-10 21:51:11 +0800

    add search thread

--- app/src/main/java/com/example/test/myapplication/MainActivity.java
+++ app/src/main/java/com/example/test/myapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.test.myapplication;
 
-import android.os.StrictMode;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -15,44 +17,84 @@ import org.jsoup.select.Elements;
 
 public class MainActivity extends AppCompatActivity {
 
+    SearchThread searchThread;
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
+
+        searchThread = new SearchThread();
+        searchThread.start();
         Button bn = (Button) findViewById(R.id.button);
-        bn.setOnClickListener(new MyClickListener());
-    }
-
-    class MyClickListener implements View.OnClickListener
-    {
-        TextView txt = (TextView) findViewById(R.id.txt);
-
-        @Override
-        public void onClick(View v)
+        bn.setOnClickListener(new View.OnClickListener()
         {
-            Document doc = null;
-            try {
-                TextView editText = (TextView) findViewById(R.id.editText);
-                String url1 = "https://search.jd.com/Search?keyword=";
-                String url2 = "&enc=utf-8&pvid=19314f6c144a42f8aeaf082d2a905af6";
-                doc = Jsoup.connect(url1 + editText.getText() + url2).get();
-                Elements items = doc.select("li.gl-item");
-                String info = "";
-                for (Element item : items) {
-                    info += item.select("div.p-price").text();
-                    info += item.select("div.p-name").select("em").text();
-                    info += "\n";
-                }
-                txt.setText(info);
-            } catch (Exception e) {
-                e.printStackTrace();
-                txt.setText("Error: " + e);
+            @Override
+            public void onClick(View v)
+            {
+                searchThread.handler.sendEmptyMessage(0);
             }
 
+        });
+        handler = new Handler()
+        {
+            @Override
+            public void handleMessage(Message msg)
+            {
+                if (msg.what == 0x0001) {
+                    TextView txt = (TextView) findViewById(R.id.txt);
+                    txt.setText(msg.getData().getString("info"));
+                }
+            }
+        };
+    }
+
+    class SearchThread extends Thread
+    {
+        public Handler handler;
+
+        @Override
+        public void run()
+        {
+            Looper.prepare();
+            handler = new Handler()
+            {
+                @Override
+                public void handleMessage(Message m)
+                {
+                    Document doc = null;
+                    String info = "";
+                    try
+                    {
+                        TextView editText = (TextView) findViewById(R.id.editText);
+                        String url1 = "https://search.jd.com/Search?keyword=";
+                        String url2 = "&enc=utf-8&pvid=19314f6c144a42f8aeaf082d2a905af6";
+                        doc = Jsoup.connect(url1 + editText.getText() + url2).get();
+                        Elements items = doc.select("li.gl-item");
+                        for (Element item : items) {
+                            info += item.select("div.p-price").text();
+                            info += item.select("div.p-name").select("em").text();
+                            info += "\n";
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        e.printStackTrace();
+                        info = "Error: " + e;
+                    }
+                    finally
+                    {
+                        Message msg = new Message();
+                        msg.what = 0x0001;
+                        Bundle bundle = new Bundle();
+                        bundle.putString("info", info);
+                        msg.setData(bundle);
+                        MainActivity.this.handler.sendMessage(msg);
+                    }
+                }
+            };
+            Looper.loop();
         }
     }
 }
