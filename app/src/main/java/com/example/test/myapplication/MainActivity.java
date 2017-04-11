5652f31434a36ebb76a74dafbdcd68c763137448
test <test@test.com>
2017-04-11 14:40:10 +0800

    inner thread

--- app/src/main/java/com/example/test/myapplication/MainActivity.java
+++ app/src/main/java/com/example/test/myapplication/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.test.myapplication;
 
 import android.os.Handler;
-import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,7 +15,6 @@ import org.jsoup.select.Elements;
 
 public class MainActivity extends AppCompatActivity {
 
-    SearchThread searchThread;
     Handler handler;
 
     @Override
@@ -25,23 +22,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchThread = new SearchThread();
-        searchThread.start();
         Button bn = (Button) findViewById(R.id.button);
-        bn.setOnClickListener(new View.OnClickListener()
-        {
+        bn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                searchThread.handler.sendEmptyMessage(0);
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Document doc;
+                        String info = "";
+                        try {
+                            TextView editText = (TextView) findViewById(R.id.editText);
+                            String url1 = "https://search.jd.com/Search?keyword=";
+                            String url2 = "&enc=utf-8&pvid=19314f6c144a42f8aeaf082d2a905af6";
+                            doc = Jsoup.connect(url1 + editText.getText() + url2).get();
+                            Elements items = doc.select("li.gl-item");
+                            for (Element item : items) {
+                                info += item.select("div.p-price").text();
+                                info += item.select("div.p-name").select("em").text();
+                                info += "\n";
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            info = "Error: " + e;
+                        } finally {
+                            Message msg = new Message();
+                            msg.what = 0x0001;
+                            Bundle bundle = new Bundle();
+                            bundle.putString("info", info);
+                            msg.setData(bundle);
+                            handler.sendMessage(msg);
+                        }
+                    }
+                }).start();
             }
 
         });
-        handler = new Handler()
-        {
+
+        handler = new Handler() {
             @Override
-            public void handleMessage(Message msg)
-            {
+            public void handleMessage(Message msg) {
                 if (msg.what == 0x0001) {
                     TextView txt = (TextView) findViewById(R.id.txt);
                     txt.setText(msg.getData().getString("info"));
@@ -50,51 +70,4 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-    class SearchThread extends Thread
-    {
-        public Handler handler;
-
-        @Override
-        public void run()
-        {
-            Looper.prepare();
-            handler = new Handler()
-            {
-                @Override
-                public void handleMessage(Message m)
-                {
-                    Document doc = null;
-                    String info = "";
-                    try
-                    {
-                        TextView editText = (TextView) findViewById(R.id.editText);
-                        String url1 = "https://search.jd.com/Search?keyword=";
-                        String url2 = "&enc=utf-8&pvid=19314f6c144a42f8aeaf082d2a905af6";
-                        doc = Jsoup.connect(url1 + editText.getText() + url2).get();
-                        Elements items = doc.select("li.gl-item");
-                        for (Element item : items) {
-                            info += item.select("div.p-price").text();
-                            info += item.select("div.p-name").select("em").text();
-                            info += "\n";
-                        }
-                    }
-                    catch(Exception e)
-                    {
-                        e.printStackTrace();
-                        info = "Error: " + e;
-                    }
-                    finally
-                    {
-                        Message msg = new Message();
-                        msg.what = 0x0001;
-                        Bundle bundle = new Bundle();
-                        bundle.putString("info", info);
-                        msg.setData(bundle);
-                        MainActivity.this.handler.sendMessage(msg);
-                    }
-                }
-            };
-            Looper.loop();
-        }
-    }
 }
