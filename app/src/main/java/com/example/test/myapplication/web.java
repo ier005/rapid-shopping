772d9ea61a8c89fa68e748b276a6de68f38fdad0
ier005 <ier005@users.noreply.github.com>
2017-04-18 15:00:04 +0800

    add sharing function

--- app/src/main/java/com/example/test/myapplication/web.java
+++ app/src/main/java/com/example/test/myapplication/web.java
@@ -5,10 +5,15 @@ package com.example.test.myapplication;
  */
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import android.support.v7.widget.ShareActionProvider;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebBackForwardList;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
@@ -17,6 +22,9 @@ import android.webkit.WebViewClient;
 public class web extends AppCompatActivity {
 
     WebView webView;
+    private ShareActionProvider mShareActionProvider;
+    Intent shareIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +38,25 @@ public class web extends AppCompatActivity {
         webView.loadUrl(intent.getStringExtra("url"));             //加载外网
         webView.setWebViewClient(new HelloWebViewClient ());
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu_share, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.action_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+
+        // Return true to display menu
+        return true;
     }
 
     @Override
@@ -60,6 +87,10 @@ public class web extends AppCompatActivity {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
+
+            shareIntent.putExtra(Intent.EXTRA_TEXT, webView.getUrl());
+            mShareActionProvider.setShareIntent(shareIntent);
+
             return true;
         }
     }
